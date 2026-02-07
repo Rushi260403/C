d837a370dc1c173cc4d54da2888faea469ec0a97
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-07 11:14:18 +0530

    saved all files

--- 01_first.c
+++ 01_first.c
@@ -1,4 +1,4 @@
-#include<stdio.h>  #include is a preprocessor directive that tells the compiler to include the contents of the specified header file. In this case, we are including the standard input/output library, which allows us to use functions like printf.
+#include<stdio.h> //include is a preprocessor directive that tells the compiler to include the contents of the specified header file. In this case, we are including the standard input/output library, which allows us to use functions like printf.
 
 int main(){
     printf("Hellow world");
