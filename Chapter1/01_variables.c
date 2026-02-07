02d1226fe0298ddeba60a20cb9992ac26be2f035
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-07 11:36:37 +0530

    initial commit

--- Chapter1/01_variables.c
+++ Chapter1/01_variables.c
@@ -1,6 +1,8 @@
 #include<stdio.h>  // include is a preprocessor directive that tells the compiler to include the contents of the specified header file. In this case, we are including the standard input/output library, which allows us to use functions like printf.
 
 int main(){
-    printf("Hellow world");
+    int a; // Variable declaration
+    a = 6; // Variable initialization
+    printf("The output of this program is %d", a);
     return 0;
 }
