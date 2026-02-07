f03b4c457874f552e8ba088fb00f5ef1bc0d5fdc
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-07 21:04:43 +0530

    Problem5 solved

---
+++ Chapter2_Practice_set/05_Problem5.c
@@ -0,0 +1,11 @@
+/*
+3.0 + 1 will be: Integer, Floating point number or Character
+*/
+
+#include<stdio.h>
+
+int main(){
+    float a =3.0 + 1;
+    printf("The value of a is %f", a);
+    return 0;
+}
