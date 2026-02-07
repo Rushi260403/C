499494e822e356350820d8a37b8b478a3ca89ca4
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-07 14:03:32 +0530

    type conversion in C

---
+++ Chapter2/03_type_conversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    float a = 9.0;
+    int b = 2;
+    float c = a/b;
+    printf("The value of a/b is %f\n",c);
+    int d = 6.7;
+    printf("The value of d is %d", d);
+    return 0;
+}
