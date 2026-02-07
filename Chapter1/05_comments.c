48d4ec211292324ab00f5c966d1fca2a33e9bb07
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-07 12:01:25 +0530

    understanding comments in c

---
+++ Chapter1/05_comments.c
@@ -0,0 +1,12 @@
+/*
+Author: Rushi
+Date: 7 February 2026
+*/
+#include<stdio.h>  // include is a preprocessor directive that tells the compiler to include the contents of the specified header file. In this case, we are including the standard input/output library, which allows us to use functions like printf.
+
+int main(){
+    // I am writing this just for fun. This line doesnt do anything.
+    printf("Hello world");
+    // Ruturn 0 is returning 0 to the operating system, which indicates that the program has executed successfully. It is a common convention in C programming to return 0 from the main function to indicate that the program has completed without errors.
+    return 0; // This is return statement
+}
