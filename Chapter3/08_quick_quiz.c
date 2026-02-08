b08c305b7d492369e8c20fa66134c6d4a701d6a4
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-08 14:17:22 +0530

    semi colon added

--- Chapter3/08_quick_quiz.c
+++ Chapter3/08_quick_quiz.c
@@ -4,22 +4,22 @@ int main(){
     char grade;
     int marks = 46;
     if(marks<=100 && marks>=91){
-        printf = 'A';
+        grade = 'A';
     }
     else if (marks<=90 && marks>=81){
-        printf = 'B';
+        grade = 'B';
     }
     else if (marks<=80 && marks>=71){
-        printf = 'C';
+        grade = 'C';
     }
     else if (marks<=70 && marks>=61){
-        printf = 'D';
+        grade = 'D';
     }
     else if (marks<=60 && marks>=51){
-        printf = 'E';
+        grade = 'E';
     }
     else{
-        printf = 'F';
+        grade = 'F';
     }
     return 0;
 }
